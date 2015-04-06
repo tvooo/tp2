4c510ac9e0449269ab8b7d7a67136f9c05cd4969
Tim von Oldenburg <tim@tvooo.de>
2015-04-06 14:26:45 +0200

    New title page

--- check.sh
+++ check.sh
@@ -2,5 +2,8 @@
 
 # https://github.com/btford/write-good
 
-write-good {0,1,9}*.md
+# https://www.linux.com/learn/tutorials/357267-using-spell-checking-in-vim 
+
+writsttimetried = now();
+e-good {0,1,9}*.md
 wc -w {0,1,9}*.md
